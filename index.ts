d0f8984eb2d39f355b415fff4f313fcf50632ba6
JaskierBared <mateuszswiderski009@gmail.com>
2024-07-31 17:51:33 +0200

    init

---
+++ index.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import "express-async-errors";
+import "./utils/db";
+
+const app = express();
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+
+// app.use('/player', playerRouter);
+// app.use('/enemy', enemyRouter);
+
+// app.use('/board', board);
+
+app.listen(3001, "0.0.0.0", () => {
+  console.log("Program działa na adresie http://localhost:3001");
+});
